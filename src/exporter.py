7d7147851fef8481a9b7c7788009902528eb7ac3
Mikhail Chinkov <michael.chinkov@gmail.com>
2026-03-04 14:18:17 +0100

    fix: properly handle slashes in the step name

--- src/exporter.py
+++ src/exporter.py
@@ -200,7 +200,9 @@ for job in job_lst:
                 with trace.use_span(child_1, end_on_exit=False):
                     # Parse logs
                     try:
-                        with open ("./logs/"+str(job["name"]).replace("/", "")+"/"+str(step['number'])+"_"+str(step['name'].replace("/",""))+".txt") as f:
+                        # Use exact job and step names from API; GitHub's zip uses them verbatim for folder/file names (see case 2777)
+                        log_path = "./logs/"+str(job["name"])+"/"+str(step['number'])+"_"+str(step['name'])+".txt"
+                        with open (log_path) as f:
                             for line in f.readlines():
                                 try:
                                     line_to_add = line[29:-1].strip()
@@ -236,7 +238,7 @@ for job in job_lst:
                             print("Log file not expected for this step ->",step['name'],"<- because its status is ->",step['conclusion'])
                             pass #We don't expect log file to exist
                         else:
-                            print("ERROR: Log file does not exist: "+str(job["name"]).replace("/", "")+"/"+str(step['number'])+"_"+str(step['name'].replace("/",""))+".txt")
+                            print("ERROR: Log file does not exist: "+str(job["name"])+"/"+str(step['number'])+"_"+str(step['name'])+".txt")
                             
 
                 if step['conclusion'] == 'skipped' or step['conclusion'] == 'cancelled':
